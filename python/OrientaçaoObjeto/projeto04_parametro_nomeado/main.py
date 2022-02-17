17788debd86a30e3d8a4007e59b511798cece2d7
Gabriel Paiva Marinho <858097@senacminas.edu.br>
2022-02-17 20:54:45 -0300

    Aula senac

--- python/OrientaçaoObjeto/projeto04_parametro_nomeado/main.py
+++ python/OrientaçaoObjeto/projeto04_parametro_nomeado/main.py
@@ -4,6 +4,33 @@ from animal import Animal
 
 os.system('cls')
 
+animal1 = Animal()
+animal2 = Animal()
+animal3 = Animal()
+
+
+tobias = Animal(nome='Tobias')
+felipe = Animal(nome='Felipe')
+bob = Animal(nome='Bob')
+
+
+print('-'*70)
+print(animal1)
+print('-'*70)
+print(animal2)
+print('-'*70)
+print(animal3)
+print('-'*70)
+print()
+
+print('-'*70)
+print(tobias)
+print('-'*70)
+print(felipe)
+print('-'*70)
+print(bob)
+print('-'*70)
+print()
 
 
  
